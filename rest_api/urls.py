c516ddcad5c38d3b4b2147df758da6f3566bdd13
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2024-03-11 20:13:53 +0330

    created show student api view

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .import views
+
 urlpatterns = [
-    
+    path('students/show',views.show_students_info.as_view()),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+from .models import Student
+from .serializer import Student_serializer
+
 # Create your views here.
+
+class show_students_info(APIView):
+    def get(self,request:Request):
+        students_query=Student.objects.all()
+        serializer=Student_serializer(instance=students_query,many=True)
+        return Response(data=serializer.data,status=status.HTTP_200_OK)
+    
