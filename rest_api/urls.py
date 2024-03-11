1efdf211c8dde15dbbed0a44760cfc96f44e59c2
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2024-03-11 21:22:53 +0330

    create show student info api view

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -5,6 +5,7 @@ from .import views
 urlpatterns = [
     # student
     path('students/show',views.show_students_info.as_view()),
+    path('students/show/<int:student_id>',views.show_student_info.as_view()),
     path('student/register',views.register_student.as_view()),
     # teacher
     path('teachers/show',views.show_teachers_info.as_view()),
--- rest_api/views.py
+++ rest_api/views.py
@@ -16,6 +16,19 @@ class show_students_info(APIView):
         students_query=Student.objects.all()
         serializer=Student_serializer(instance=students_query,many=True)
         return Response(data=serializer.data,status=status.HTTP_200_OK)
+    
+class show_student_info(APIView):
+    def get_object(self,student_id:int):
+        try:
+            selected_student=Student.objects.get(pk=student_id)
+            return selected_student
+        except Student.DoesNotExist:
+            return Response(data=None,status=status.HTTP_404_NOT_FOUND)
+    def get(self, request:Request,student_id:int):
+        selected_student=self.get_object(student_id)
+        serializer=Student_serializer(instance=selected_student)
+        return Response(data=serializer.data,status=status.HTTP_200_OK)
+        
 
 class register_student(mixins.CreateModelMixin,generics.GenericAPIView):
     queryset=Student.objects.all()
