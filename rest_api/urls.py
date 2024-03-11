46b6b535c96765cbfc134fd029862da599258105
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2024-03-11 22:00:56 +0330

    create student info api view

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # student
     path('students/show',views.show_students_info.as_view()),
     path('students/show/<int:student_id>',views.show_student_info.as_view()),
+    path('students/edit/<int:student_id>',views.edit_student_info.as_view()),
     path('student/register',views.register_student.as_view()),
     # teacher
     path('teachers/show',views.show_teachers_info.as_view()),
--- rest_api/views.py
+++ rest_api/views.py
@@ -28,7 +28,25 @@ class show_student_info(APIView):
         selected_student=self.get_object(student_id)
         serializer=Student_serializer(instance=selected_student)
         return Response(data=serializer.data,status=status.HTTP_200_OK)
-        
+    
+class edit_student_info(APIView):
+    def get_object(self,student_id:int):
+        try:
+            selected_student=Student.objects.get(pk=student_id)
+            return selected_student
+        except Student.DoesNotExist:
+            return Response(data=None,status=status.HTTP_404_NOT_FOUND)
+    def get(self, request:Request,student_id:int):
+        selected_student=self.get_object(student_id)
+        serializer=Student_serializer(instance=selected_student)
+        return Response(data=serializer.data,status=status.HTTP_200_OK)
+    def put(self, request:Request,student_id:int):
+        selected_student=self.get_object(student_id)
+        edit=Student_serializer(instance=selected_student,data=request.data)
+        if edit.is_valid():
+            edit.save()
+            return Response(data=edit.data,status=status.HTTP_202_ACCEPTED)
+        return Response(data=None,status=status.HTTP_400_BAD_REQUEST)
 
 class register_student(mixins.CreateModelMixin,generics.GenericAPIView):
     queryset=Student.objects.all()
